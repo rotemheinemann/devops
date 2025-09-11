ebd2921da586d82918491002c731b87c84fddc94
Your Name <you@example.com>
2025-09-11 13:41:52 +0300

    test app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.get("/")
 def home():
-    return "Hey! how are you?"
+    return "מה קורה צדיק?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
